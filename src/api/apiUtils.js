aefb3d8c61cf37d34dd5f64cd5ba21691b1ee3ac
danil <danil.scheglov@mail.ru>
2023-12-21 14:35:40 +0300

    final_strokes

--- src/api/apiUtils.js
+++ src/api/apiUtils.js
@@ -3,8 +3,8 @@ import axios from "axios";
 import jwtDecode from 'jwt-decode';
 import getUserInfoFromToken from "../Utils/Functions/getUserInfoFromToken";
 
-// export const baseServerURL = 'https://yomessage-api.ru'
-const baseServerURL = 'http://localhost:5050'
+export const baseServerURL = 'https://yomessage-api.ru'
+// const baseServerURL = 'http://localhost:5050'
 
 const instance = axios.create({
     baseURL: `${baseServerURL}/api`, // URL сервера API
